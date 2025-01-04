40b3fa2964e871f534b1706097e36f7b5c1135e6
muhammed <mmkocak6565@gmail.com>
2025-01-04 20:20:58 +0300

    bozuk mixinler silindi card da bulunan kaydırma hatası düzeltildi

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -35,7 +35,7 @@ class HomeScreen extends StatelessWidget {
         'price': 12.50,
         'image': 'assets/images/alpino.png',
       },
-       {
+      {
         'name': 'Cappucino',
         'description': 'com Chocolate',
         'price': 11.50,
@@ -64,75 +64,73 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: screenHeight * 0.3,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color(0xFF8B4513),
-                        Color(0xFFB17445),
-                      ],
-                    ),
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: screenWidth * 0.05,
-                    vertical: screenHeight * 0.01,
-                  ),
-                  child: SafeArea(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(top: screenHeight * 0.02),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    'Bem-vindo',
-                                    style: TextStyle(
-                                      color: Color(0xFFB7B7B7),
-                                      fontSize: screenWidth * 0.035,
-                                    ),
-                                  ),
-                                  SizedBox(height: screenHeight * 0.005),
-                                  Text(
-                                    'Fulano de Tal',
-                                    style: TextStyle(
-                                      color: Color(0xFFDDDDDD),
-                                      fontSize: screenWidth * 0.05,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              CircleAvatar(
-                                radius: screenWidth * 0.06,
-                                backgroundColor: Colors.white,
-                                child: Icon(
-                                  Icons.person,
-                                  color: Color(0xFFB17445),
-                                  size: screenWidth * 0.08,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+          Column(
+            children: [
+              Container(
+                height: screenHeight * 0.3,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color(0xFF8B4513),
+                      Color(0xFFB17445),
+                    ],
                   ),
                 ),
-                SizedBox(height: screenHeight * 0.05),
-                Container(
-                  height: screenHeight * 0.7,
+                padding: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.05,
+                  vertical: screenHeight * 0.01,
+                ),
+                child: SafeArea(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: screenHeight * 0.02),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'Bem-vindo',
+                                  style: TextStyle(
+                                    color: Color(0xFFB7B7B7),
+                                    fontSize: screenWidth * 0.035,
+                                  ),
+                                ),
+                                SizedBox(height: screenHeight * 0.005),
+                                Text(
+                                  'Fulano de Tal',
+                                  style: TextStyle(
+                                    color: Color(0xFFDDDDDD),
+                                    fontSize: screenWidth * 0.05,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            CircleAvatar(
+                              radius: screenWidth * 0.06,
+                              backgroundColor: Colors.white,
+                              child: Icon(
+                                Icons.person,
+                                color: Color(0xFFB17445),
+                                size: screenWidth * 0.08,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
                   padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
                   child: RawScrollbar(
                     thumbColor: Color(0xFFB17445),
@@ -162,8 +160,8 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
           Positioned(
             top: screenHeight * 0.22,
@@ -178,4 +176,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-} 
+}
